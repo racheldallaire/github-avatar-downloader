d506fbe07e120ca98cb0737c585acf6be25c33ba
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-01-22 21:00:54 +0000

    hid private authentication key

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,29 @@
+var token = require('./secrets.js');
+
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+
+  });
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
