1173cc5ef67ab5954c6542d645cf957201d4f3ab
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-01-23 01:25:41 +0000

    Required user input

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,23 @@
-var token = require('./secrets.js');
+//Required modules
 
+var token = require('./secrets.js');
 var request = require('request');
 var fs = require('fs');
 
+//Welcome message
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//Function for getting URL from command line input
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var url = "https://api.github.com/repos/" + process.argv[2] + "/" + process.argv[3] + "/contributors";
+
+//If statements to require user input
+  if (!process.argv[2] || !process.argv[3]) {
+  console.log('Error: Please enter a valid repo owner and/or name.');
+  return;
+  }
 
   var options = {
     url: "https://api.github.com/repos/" + process.argv[2] + "/" + process.argv[3] + "/contributors",
@@ -16,11 +27,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+//Making sure the data is accessed as a JSON object rather than a massive string
+
   request(options, function(err, res, body) {
     cb(err, JSON.parse(body));
   });
 
 }
+
+//Iterating through the array to access variables needed for callback function
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
@@ -34,9 +49,9 @@ getRepoContributors("jquery", "jquery", function(err, result) {
       downloadImageByURL(youareL, path);
   }
 
-
-
 });
+
+//Callback function used to download image, gives output for user readability
 
 function downloadImageByURL(url, filePath) {
 
